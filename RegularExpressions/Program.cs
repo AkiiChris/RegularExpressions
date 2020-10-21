6043d72143d27e9b91f2eb8d8dc6a69a87c5f3aa
akii <akiichris01@gmail.com>
2020-10-21 00:12:42 -0400

    commit

---
+++ RegularExpressions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegularExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        public static string Name_Regex = "^[A-Z]{1}[A-Za-z]{30,}$";
+        public static string Email_Regex = "^[6-9]{1}[0-9]{9}$";
+        public static string PhoneNumb_Regex = "^[0-9a-zA-Z]+[.+-]{0,1}[0-9a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-z]{2,3}){,1}";
+        public static string Month_Regex = "";
+    }
+    
+
+       
+    
+        public static bool ValidateName()
+        {
+            string text = "Please enter a valid name:";
+            Console.WriteLine(text);
+            if (!Regex.Match(text, "^[A - Z]{ 1}[A-Za - z]{ 30,}*$").Success) 
+            {
+                Console.WriteLine("Sorry name not valid.");
+            }
+
+        }    
+    }
+}
+
---
+++ RegularExpressions/RegexValidation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RegularExpressions
+{
+    internal class RegexValidation
+    {
+        private bool ValidateName;
+
+        public RegexValidation()
+        {
+            RegexValidation regex = new RegexValidation();
+            bool validResult = regex.ValidateName
+            Console.WriteLine(validResult);
+        }
+    }
+}
